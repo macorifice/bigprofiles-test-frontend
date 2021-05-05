31a23249a2b9e25e512d2c699536313e16fc4acc
Stefano Di Cecco <s.dicecco@reply.it>
2021-05-05 17:37:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Avvertenze
+Usare le due cartelle "backend" e "frontend" come root dei due componenti da sviluppare.
+
+Lo script "import_data.sh" all'interno della cartella "bin" permette di effettaure le chiamate api. Per lanciarlo usare il comando "bin/import_data.sh" o "sh bin/import_data.sh".
+
---
+++ bin/import_data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+echo "Start 100k api calls..."
+for i in {1..100000}; do 
+	if [ $(( $i % 10000 )) -eq 0 ] ; then
+		echo $i" calls done!"
+	fi
+	JOB=$((1 + $RANDOM % 4))
+	DAY=$((1 + $RANDOM % 30))
+	DATE="2021-04-"$DAY
+	curl -s -XPOST 'localhost:3000/api/v1.0/value' --header 'Content-Type: application/json' --data-raw '{
+    "data": "$DATE",
+    "job": "$JOB"
+	}' > /dev/null
+done
